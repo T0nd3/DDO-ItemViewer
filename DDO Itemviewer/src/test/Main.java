b22ad05a5be2043ae4585e6cd6f094f1489e5b40
T0nd3 <T0nd3@DDOV>
2017-04-18 15:47:39 +0200

    update

---
+++ DDO Itemviewer/src/test/Main.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		ArchievmentReader reader = new ArchievmentReader();
+		reader.read();
+	}
+}
