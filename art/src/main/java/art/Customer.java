53bbdb38db0653aec990acee196a067a0c71c3c4
T0nd3 <T0nd3@DDOV>
2017-05-04 01:27:47 +0200

    add new Proj

---
+++ art/src/main/java/art/Customer.java
@@ -0,0 +1,36 @@
+package art;
+
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Customer {
+
+	private String firstName, lastName, email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
